132898675c5d0b956517f4121a619daf38b4cab6
miguel candelario barrones <mangel64578@gmail.com>
2024-04-19 14:00:17 +0200

    first commit

---
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { environment } from 'src/environments/environment';
+
+
+
+@Component({
+  templateUrl: './full-screen-page.component.html',
+  styleUrls: ['./full-screen-page.component.css']
+})
+export class FullScreenPageComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      accessToken:environment.mapbox_key,
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
+
+
+}
